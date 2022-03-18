e5865763b1d5e57413a06662b0aeed0056c6231e
Ammar Elbasheer <elbashammar@outlook.com>
2022-03-17 20:30:28 -0700

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,90 @@
+﻿using YoutubeExplode;
+using YoutubeExplode.Videos.Streams;
+using YoutubeExplode.DemoConsole.Utils;
+using YoutubeExplode.Videos;
+string userName = Environment.UserName;
+string videoId;
+
+var youtube = new YoutubeClient();
+while (true)
+{
+    Console.WriteLine("Welcome to youtube video downloader. Input the youtube video link or ID that you would like to download.");
+
+    try
+    { 
+        videoId = VideoId.Parse(Console.ReadLine() ?? "");
+        var video1 = await youtube.Videos.GetAsync(videoId);
+        break;
+    }
+    catch (System.ArgumentException)
+    {
+        Console.WriteLine("That is an invalid video URL or ID, try again.");
+    }
+
+}
+
+var video = await youtube.Videos.GetAsync(videoId);
+var streamManifest = await youtube.Videos.Streams.GetManifestAsync(videoId);
+Console.WriteLine($"\nTitle: {video.Title}\nChannel: {video.Author.Title}\nDuration: {video.Duration}");
+Console.WriteLine("\nWould you like MUXED, VIDEO only, or AUDIO only? (M/V/A) \n*Muxed video download is capped at 720p30.");
+
+while (true)
+{
+    string readOnly1 = Console.ReadLine();
+
+    if (readOnly1 == "M" || readOnly1 == "m")
+    {
+        var streamInfo = streamManifest.GetMuxedStreams().GetWithHighestVideoQuality();
+        Console.Write($"Downloading stream: {streamInfo.VideoQuality.Label} / {streamInfo.Container.Name}... ");
+
+        var fileName = @$"C:\Users\{userName}\Downloads\{video.Title}.{streamInfo.Container.Name}";
+
+        using (var progress = new InlineProgress())
+            await youtube.Videos.Streams.DownloadAsync(streamInfo, fileName, progress);
+        Console.WriteLine($"Video saved to '{fileName}'");
+        break;
+    }
+
+    if (readOnly1 == "A" || readOnly1 == "a")
+    {
+        var streamInfo = streamManifest.GetAudioOnlyStreams().GetWithHighestBitrate();
+        Console.Write($"Downloading stream: {streamInfo.Bitrate} / {streamInfo.Container.IsAudioOnly}... ");
+
+        var fileName = @$"C:\Users\{userName}\Downloads\{video.Title}.mp3";
+
+        using (var progress = new InlineProgress())
+            await youtube.Videos.Streams.DownloadAsync(streamInfo, fileName, progress);
+
+        Console.WriteLine($"Video saved to '{fileName}'");
+        break;
+    }
+
+    if (readOnly1 == "V" || readOnly1 == "v")
+    {
+        var streamInfo = streamManifest
+            .GetVideoOnlyStreams()
+            .Where(s => s.Container == Container.Mp4)
+            .GetWithHighestVideoQuality();
+        Console.Write($"Downloading stream: {streamInfo.VideoQuality.Label} / {streamInfo.Container.Name}... ");
+
+        var fileName = @$"C:\Users\{userName}\Downloads\{video.Title}.{streamInfo.Container.Name}";
+
+        using (var progress = new InlineProgress())
+            await youtube.Videos.Streams.DownloadAsync(streamInfo, fileName, progress);
+
+        Console.WriteLine($"Video saved to '{fileName}'");
+        break;
+    }
+
+    else
+    {
+        Console.WriteLine("That is not a valid input, please try again.");
+        Console.WriteLine("\nWould you like MUXED, VIDEO only, or AUDIO only? (M/V/A) \n*Muxed video download is capped at 720p30\n");
+
+    }
+
+}
+
+Console.WriteLine("Press any key to exit.");
+Console.ReadLine();
+
---
+++ ConsoleApp2/YTprogress.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoutubeExplode.DemoConsole.Utils;
+
+internal class InlineProgress : IProgress<double>, IDisposable
+{
+    private readonly int _posX;
+    private readonly int _posY;
+
+    public InlineProgress()
+    {
+        _posX = Console.CursorLeft;
+        _posY = Console.CursorTop;
+    }
+
+    public void Report(double progress)
+    {
+        Console.SetCursorPosition(_posX, _posY);
+        Console.WriteLine($"{progress:P1}");
+    }
+
+    public void Dispose()
+    {
+        Console.SetCursorPosition(_posX, _posY);
+        Console.WriteLine("Completed ✓");
+    }
+}
